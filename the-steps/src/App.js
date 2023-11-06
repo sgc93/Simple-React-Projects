b5101d32ea5ad47389cb85fc47be2e1da49a0c7c
sgc93 <smachewgedefaw@gmail.com>
2023-11-05 23:19:54 -0900

    create components

--- the-steps/src/App.js
+++ the-steps/src/App.js
@@ -1,3 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.js";
+const messages = [
+	"Learn React ⚛️",
+	"Apply for jobs 💼",
+	"Invest your new income 🤑",
+];
+
+export default function App() {
+	const step = 1;
+	return (
+		<div className="steps">
+			<Header />
+			<div className="numbers">
+				<div className={`${step >= 1 ? "active" : ""} `}>1</div>
+
+				<div className={`${step >= 2 ? "active" : ""} `}>2</div>
+
+				<div className={`${step >= 3 ? "active" : ""} `}>3</div>
+			</div>
+			<p className="message">{messages[step - 1]}</p>
+			<div className="buttons">
+				<button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+					Previous
+				</button>
+				<button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+					Next
+				</button>
+			</div>
+		</div>
+	);
+}
+
+function Header() {
+	return (
+		<h3>
+			Here is a breakdown of core points to learn in React.js, categorized into
+			different proficiency levels _____ By chatGPT
+		</h3>
+	);
+}

31efed1b36743965a2794e4ebf02bc30ebbbcd78
sgc93 <smachewgedefaw@gmail.com>
2023-11-05 23:32:13 -0900

    destructuring components

--- the-steps/src/App.js
+++ the-steps/src/App.js
@@ -9,21 +9,10 @@ export default function App() {
 	return (
 		<div className="steps">
 			<Header />
-			<div className="numbers">
-				<div className={`${step >= 1 ? "active" : ""} `}>1</div>
-
-				<div className={`${step >= 2 ? "active" : ""} `}>2</div>
-
-				<div className={`${step >= 3 ? "active" : ""} `}>3</div>
-			</div>
-			<p className="message">{messages[step - 1]}</p>
-			<div className="buttons">
-				<button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
-					Previous
-				</button>
-				<button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
-					Next
-				</button>
+			<div className="content">
+				<Nubmers step={step} />
+				<p className="message">{messages[step - 1]}</p>
+				<Buttons />
 			</div>
 		</div>
 	);
@@ -35,5 +24,30 @@ function Header() {
 			Here is a breakdown of core points to learn in React.js, categorized into
 			different proficiency levels _____ By chatGPT
 		</h3>
+	);
+}
+
+function Nubmers({ step }) {
+	const remainingSteps = 40;
+	return (
+		<div className="numbers">
+			<div className="learned-steps active">1</div>
+			<div className="remain-step">
+				Remaining <span className="active">{remainingSteps}</span>
+			</div>
+		</div>
+	);
+}
+
+function Buttons() {
+	return (
+		<div className="buttons">
+			<button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+				Previous
+			</button>
+			<button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+				Next
+			</button>
+		</div>
 	);
 }
